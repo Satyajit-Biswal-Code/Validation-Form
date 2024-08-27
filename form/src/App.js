56ef6cd6ba5d84177a7a5e7f79e2e300042b8326
Satyajit-Biswal-code <satyajitbiswal2580@gmail.com>
2024-08-27 14:59:16 +0530

    Validation form

---
+++ form/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="full">
+    <form>
+      <h1>REGISTRATION FROM</h1>
+      <label>Name</label>
+      <input type="text" placeholder="Enter Your Name"></input>
+      <label>Email</label>
+      <input type="email" placeholder="Enter Your Email"></input>
+      <label>Password</label>
+      <input type="password" placeholder="Enter Your Password"></input>
+      <button>Register</button>
+    </form></div>
+    </div>
+  );
+}
+
+export default App;
